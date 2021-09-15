1b71ceb3b5ae3cc2e04de51b8af000da93670a27
darkbazory <73275943+darkbazory@users.noreply.github.com>
2021-09-15 14:24:48 +0200

    js

---
+++ jc/Menu2.js
@@ -0,0 +1,7 @@
+const btnMenu = document.querySelector("#btnMenu");
+const menu = document.querySelector("#menu");
+btnMenu.addEventListener("click", function(){
+    menu.classList.toggle("mostrar");
+});
+
+const subMenu = document.querySelector("#btnMenu");
